846eddd1692071cb9c5ccb76f5458ada89f3abed
Janardhanam Subramaniam <janardhanamsubramaniam@Janardhanams-MacBook-Air.local>
2026-02-20 11:50:36 +0530

    12th program

---
+++ 11. Turtle Graphic/main.py
@@ -0,0 +1,12 @@
+import another_module
+from turtle import Turtle, Screen
+
+print(another_module.xyz)
+
+a = Turtle()
+print(a)
+a.shape("turtle")
+a.color("coral")
+myscreen = Screen()
+print(myscreen.canvheight)
+myscreen.exitonclick()
